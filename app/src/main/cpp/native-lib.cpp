9041a41469fa77f0906dbc2ae5c57b0895a556aa
bamboolife <core_it@163.com>
2019-12-31 20:44:13 +0800

    Initial commit

---
+++ app/src/main/cpp/native-lib.cpp
@@ -0,0 +1,23 @@
+#include <jni.h>
+#include <string>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+#include "SDL_main.h"
+#include "SDL_log.h"
+#include "SDL.h"
+
+
+int main(int argc, char *argv[]){
+    struct SDL_Window *window = NULL;
+    struct SDL_Renderer *render = NULL;
+    struct SDL_Surface *bmp = NULL;
+    struct SDL_Texture *texture = NULL;
+}
+
+
+
+#ifdef __cplusplus
+}
+#endif
